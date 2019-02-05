4cef3cf70d1d94e6a75c98258c9b5fd0265ff6dc
Anna Guo <annaguo03@gmail.com>
2019-02-04 20:19:26 -0500

    Magic 8 Ball App: - Shows a fortune when button is pressed!

--- magicEightBall/ViewController.swift
+++ magicEightBall/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    var index = -1
+    let results = ["It is certain",
+                   "It is decidedly so",
+                   "Without a doubt",
+                   "Yes, definitely",
+                   "You may rely on it",
+                   "As I see it, yes",
+                   "Most likely",
+                   "Outlook good",
+                   "Yes",
+                   "Signs point to yes",
+                   "Reply hazy try again",
+                   "Ask again later",
+                   "Better not tell you now",
+                   "Cannot predict now",
+                   "Concentrate and ask again",
+                   "Don't count on it",
+                   "My reply is no",
+                   "My sources say no",
+                   "Outlook not so good",
+                   "Very doubtful"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func yesNoButtonPressed(_ sender: UIButton) {
+        var newIndex = -1
+        repeat {
+            newIndex = Int.random(in: 0..<results.count)
+        } while index == newIndex
+        
+        index = newIndex
+        answerLabel.text = results[index]
+    }
+    
 }
 
